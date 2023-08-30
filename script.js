b01693ded83c18614998efec98bb3682e927d688
Zoe <114508304+zgawlak@users.noreply.github.com>
2023-08-30 18:24:23 -0400

    Add script to image gallery

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
+// image slider
 const slides = document.querySelectorAll('.slide');
 const prevButton = document.querySelector('.prev');
 const nextButton = document.querySelector('.next');
 const dots = document.querySelectorAll('.dot');
+
+// image gallery
+const imageItems = document.querySelectorAll('.gallery-img');
+const overlay = document.querySelector('.overlay');
+const overlayImage = document.querySelector('.overlay-img');
+const closeBtn = document.querySelector('.close-btn');
+const nextBtn = document.querySelector('.next-btn');
+const prevBtn = document.querySelector('.prev-btn');
+const counter = document.querySelector('.counter');
 
 let currentSlide = 0;
 let timer;
@@ -45,3 +55,55 @@ dots.forEach((dot, index) => {
 prevButton.addEventListener('click', prevSlide);
 nextButton.addEventListener('click', nextSlide);
 goToSlide(currentSlide);
+
+
+// image gallery
+let currentIndex = 0;
+
+function updateOverlay(index) {
+    const imageUrl = imageItems[index].querySelector('img').src;
+    overlayImage.src = imageUrl;
+    counter.textContent = `${index + 1} / ${imageItems.length}`;
+    updateButtons();
+}
+
+function updateButtons() {
+    nextBtn.disabled = currentIndex === imageItems.length - 1;
+    prevBtn.disabled = currentIndex === 0;
+}
+
+imageItems.forEach((item, index) => {
+    item.addEventListener('click', (e) => {
+        e.preventDefault();
+        currentIndex = index;
+        updateOverlay(currentIndex);
+        overlay.style.display = 'block';
+        document.body.style.overflow = 'hidden';
+    });
+});
+
+closeBtn.addEventListener('click', () => {
+    overlay.style.display = 'none';
+    document.body.style.overflow = 'auto';
+});
+
+overlay.addEventListener('click', (event) => {
+    if (event.target === overlay) {
+        overlay.style.display = 'none';
+        document.body.style.overflow = 'auto';
+    }
+});
+
+function showNextImage() {
+    currentIndex = (currentIndex + 1) % imageItems.length;
+    updateOverlay(currentIndex);
+}
+
+function showPrevImage() {
+    currentIndex = (currentIndex - 1 + imageItems.length) % imageItems.length;
+    updateOverlay(currentIndex);
+}
+
+nextBtn.addEventListener('click', showNextImage);
+prevBtn.addEventListener('click', showPrevImage);
+updateOverlay(currentIndex);
